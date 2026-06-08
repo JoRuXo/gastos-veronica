a2970ada46995d9c8d9af3333d46723d3090a6c1
JoRuXo <JoRuXo@hotmail.com>
2026-06-08 19:54:37 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+var CACHE = 'mis-gastos-v2';
+var FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) {
+      return Promise.all(FILES.map(function (f) {
+        return c.add(f).catch(function () {});
+      }));
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  var url = new URL(e.request.url);
+  if (url.hostname === 'api.anthropic.com' || url.hostname.indexOf('googleapis.com') > -1 || url.hostname.indexOf('gstatic.com') > -1) return;
+  e.respondWith(
+    caches.match(e.request).then(function (cached) {
+      return cached || fetch(e.request).then(function (res) {
+        if (res && res.status === 200 && res.type === 'basic') {
+          var clone = res.clone();
+          caches.open(CACHE).then(function (c) { c.put(e.request, clone); });
+        }
+        return res;
+      }).catch(function () { return cached; });
+    })
+  );
+});
